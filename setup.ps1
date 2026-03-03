21d73af1ae3425e42e6b87fdbf25c2280ebaae5f
Keith <8584184+git-kv@users.noreply.github.com>
2026-03-03 15:47:27 -0600

    install shortcuts to public desktop and global startup programs

--- setup.ps1
+++ setup.ps1
@@ -6,14 +6,22 @@ $source_ps2exe_executables_path = "./ps2exe_executables/*"
 $source_assets_path = "./dart_app_launcher/assets/*"
 $source_application_launcher_exe = "./dart_app_launcher/dist/dart_application_launcher.exe"
 
+$shortcut = "./Dart Application Launcher.lnk"
+
+$desktop = "C:\Users\Public\Desktop"
+$startup = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+
 if (-not (Test-Path -Path $main_folder)) { New-Item -Path $main_folder -ItemType Directory -Force}
 
 if (-not (Test-Path -Path $dart_application_launcher_folder)) { New-Item -Path $dart_application_launcher_folder -ItemType Directory -Force}
 
 if (-not (Test-Path -Path $dart_application_launcher_assets_folder)) { New-Item -Path $dart_application_launcher_assets_folder -ItemType Directory -Force}
 
-Copy-Item $source_ps2exe_executables_path $main_folder
+Copy-Item $source_ps2exe_executables_path $main_folder -Force
 
-Copy-Item $source_assets_path $dart_application_launcher_assets_folder
+Copy-Item $source_assets_path $dart_application_launcher_assets_folder -Force
 
-Copy-Item $source_application_launcher_exe $dart_application_launcher_folder
+Copy-Item $source_application_launcher_exe $dart_application_launcher_folder -Force
+
+Copy-Item $shortcut $desktop -Force
+Copy-Item $shortcut $startup -Force
